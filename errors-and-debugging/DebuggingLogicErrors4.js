f1daaf4a723924ba112beb8ce81c5ce294b4d1ef
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-01-11 17:14:17 -0600

    chapter 6 exercises
    completed and uploaded

---
+++ errors-and-debugging/DebuggingLogicErrors4.js
@@ -0,0 +1,39 @@
+// Now consider both if/else blocks together (keeping the added console.log lines). 
+// Run the code and examine the output. 
+
+// Given the values for fuelLevel, crewStatus and computerStatus, should launchReady be true or false?
+// Is the program behaving as expected?
+
+let launchReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+console.log("launchReady = ", launchReady);
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+console.log("launchReady = ", launchReady);
+
+//false then true, which is logical, but not the intended output. launchready is being changed from false to true. this is a problem. 
+
+// if (launchReady) {
+//    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+//    console.log('Liftoff!');
+// } else {
+//    console.log('Launch scrubbed.');
+// }
