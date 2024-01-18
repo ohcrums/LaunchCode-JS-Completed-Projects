33978ea6577da087b373939ec091defc3d556913
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-01-18 12:31:41 -0600

    finished strings part one

--- Class-3-Exercises-Strings-Arrays/stringing-characters-together/part-one.js
+++ Class-3-Exercises-Strings-Arrays/stringing-characters-together/part-one.js
@@ -1,10 +1,44 @@
 let num = 1001;
 
+console.log(String(num))
+
 //Returns 'undefined'.
-console.log(num.length);
+//console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+console.log(String(num).length)
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
+let numDec = 123.45
+console.log(String(numDec).length-1)
+
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+// console.log(
+//     `Is 'num' a decimal? ${
+//     (String(num).includes("."))
+// }`
+// )
+
+
+if (String(num).includes(".")) {
+    console.log(`${num} is a decimal, its length is ${
+        (String(num).length-1)
+        } digits`);
+}   else {
+    console.log(`${num} is not a decimal, its length is ${
+        (String(num).length)
+        } digits.`)
+}
+
+
+if (String(numDec).includes(".")) {
+    console.log(`${numDec} is a decimal, its length is ${
+        (String(numDec).length-1)
+        } digits`);
+}   else {
+    console.log(`${numDec} is not a decimal, its length is ${
+        (String(numDec).length)
+        } digits.`)
+}
+
