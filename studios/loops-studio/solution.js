22b0c8dee1f40a7b6eb9c441d6221432ca376f72
ohCrud <ohcrud@crudbook.lan>
2024-01-22 21:45:36 -0600

    cleanup

--- studios/loops-studio/solution.js
+++ studios/loops-studio/solution.js
@@ -70,16 +70,16 @@ function runProgram() {
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
 
-  let password1 = 'LaunchCode';
-  let password2 = 'CodeLaunch';
-  console.log("Time to run the password generator so we can update the menu tomorrow.")
-  console.log(`The new password is: ${generatePassword(password1, password2)}`);
+  // let password1 = 'LaunchCode';
+  // let password2 = 'CodeLaunch';
+  // console.log("Time to run the password generator so we can update the menu tomorrow.")
+  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
 
 module.exports = {
