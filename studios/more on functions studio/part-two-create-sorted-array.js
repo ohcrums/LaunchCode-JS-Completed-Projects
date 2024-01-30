6e6092c72b207caa3f50e125b0d5eba60b2e8ae4
ohCrud <ohcrud@crudbook.lan>
2024-01-29 21:06:39 -0600

    progress

--- studios/more on functions studio/part-two-create-sorted-array.js
+++ studios/more on functions studio/part-two-create-sorted-array.js
@@ -1,12 +1,26 @@
 function findMinValue(arr){
   let min = arr[0];
+  let minArr = [];
   for (i = 0; i < arr.length; i++){
     if (arr[i] < min){
       min = arr[i];
+      minArr = [arr[i]];
     }
   }
+  arr.splice(arr.indexOf(min), 1);
+  console.log(minArr);
   return min;
 }
+
+let nums1 = [5, 10, 2, 42];
+let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+
+console.log(findMinValue(nums1));
+console.log(findMinValue(nums2));
+console.log(findMinValue(nums3));
+
 
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 
@@ -24,6 +38,6 @@ function findMinValue(arr){
  */
 
 //Sample arrays for testing:
-let nums1 = [5, 10, 2, 42];
-let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
-let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+// let nums1 = [5, 10, 2, 42];
+// let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+// let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
