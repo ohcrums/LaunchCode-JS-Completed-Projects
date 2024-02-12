071b449fa9bff2ecae0acb22240ba6ff9af4e196
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-02-12 15:19:35 -0600

    gradeLabs try/catch done

--- exercises/exceptions/test-student-labs.js
+++ exercises/exceptions/test-student-labs.js
@@ -1,8 +1,16 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    try {
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+
+    } catch(err) {
+        result = err
+        console.log(`\ncatch error thrown here!\n${lab.student} object returns: ${result}\n`);
+    } finally {
+      console.log('\nfinally here!\n')
+    }
   }
 }
 
@@ -22,3 +30,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
