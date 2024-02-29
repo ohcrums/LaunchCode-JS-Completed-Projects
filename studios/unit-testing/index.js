9bc904273303a24eb4265c8f76f062d40db8d07d
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-02-29 14:56:48 -0600

    done

--- studios/unit-testing/index.js
+++ studios/unit-testing/index.js
@@ -1,7 +1,35 @@
 
 let launchcode = {
-  
+  organization: 'nonprofit',
+  executiveDirector: 'Jeff',
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput(num) {
+    let output = '';
+
+
+    if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+      output = 'LaunchCode Rocks!';
+    } else if (num % 2 === 0 && num % 3 === 0) {
+      output = 'LaunchCode!';
+    } else if (num % 2 === 0 && num % 5 === 0) {
+      output = 'Launch Rocks!';
+    } else if (num % 3 === 0 && num % 5 === 0) {
+      output = 'Code Rocks!';
+    } else if (num % 2 === 0) {
+      output = 'Launch!';
+    } else if (num % 3 === 0) {
+      output = 'Code!';
+    } else if (num % 5 === 0) {
+      output = 'Rocks!';
+    } else {
+      output = "Rutabagas! That doesn't work";
+    }
+
+    return output;
+  }
 }
 
 module.exports = launchcode;
 
+  
--- studios/unit-testing/tests/launchcode.test.js
+++ studios/unit-testing/tests/launchcode.test.js
@@ -4,5 +4,41 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test('object key "organization" should contain value "nonprofit"', function() {
+    expect(launchcode.organization).toBe('nonprofit')
+  })
+
+  test('object key "executiveDirector" should contain value "Jeff"', function() {
+    expect(launchcode.executiveDirector).toBe('Jeff')
+  })
+
+  test('object key "percentageCoolEmployees" should contain value "100"', function() {
+    expect(launchcode.percentageCoolEmployees).toBe(100)
+  })
+
+  test('object key array "programsOffered" should contain 3 expected values', function() {
+    expect(launchcode.programsOffered).toContain('Web Development')
+    expect(launchcode.programsOffered).toContain('Data Analysis')
+    expect(launchcode.programsOffered).toContain('Liftoff')
+    expect(launchcode.programsOffered.length).toBe(3)
+  })
+
+  test('object method launchOutput has expected outputs', function() {
+    expect(launchcode.launchOutput(4)).toBe('Launch!')
+
+    expect(launchcode.launchOutput(9)).toBe('Code!')
+
+    expect(launchcode.launchOutput(25)).toBe('Rocks!')
+
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!')
+
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!')
+
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks!')
+
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!')
+
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas! That doesn't work")
+  })
+
 });
